83bd23f300bc816d9f201bf738177ce81acc6e25
princeDoria <pjDoria@mcm.edu.ph>
2024-03-14 07:08:45 +0800

    allow sending emails

--- d5Script.js
+++ d5Script.js
@@ -99,3 +99,7 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+//show captcha
+function captcha(){
+    document.getElementById("h-captcha").style.display = "block";
+}
